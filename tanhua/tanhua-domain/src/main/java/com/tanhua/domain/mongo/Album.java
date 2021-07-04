8317de11fb0ad13816f8b66bff7444d9fa61e7f3
halulu <halulume@163.com>
2021-07-04 10:43:52 +0800

    测灵魂功能 1、测试之前必须运行tanhua-service中的测试数据TestSoulFileTest 获取3套问卷。 2、在阅读代码之前必须得先了解mongodb中有什么数据，每个表对应的是什么数据。

--- tanhua/tanhua-domain/src/main/java/com/tanhua/domain/mongo/Album.java
+++ tanhua/tanhua-domain/src/main/java/com/tanhua/domain/mongo/Album.java
@@ -7,7 +7,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
- * 相册表，用于存储自己发布的数据，每一个用户一张表进行存储
+ * 相册表，用于存储自己发布的数据，每一个用户一张表进行存储，。。。
  */
 @Data
 @NoArgsConstructor
